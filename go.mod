b06a90451db55f6110917d77a09fd08d87cab7d8
Giampaolo Tranchida <giampaolo@tranchida.ch>
2025-08-17 00:55:36 +0200

    feat: implement dynamic graph generation and HTMX auto-refresh for graphs

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module htmxtest
 go 1.24
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/wcharczuk/go-chart/v2 v2.1.2 // indirect
+	golang.org/x/image v0.18.0 // indirect
+)
--- internal/handlers/pages.go
+++ internal/handlers/pages.go
@@ -1,11 +1,15 @@
 package handlers
 
 import (
+	"bytes"
 	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
 
 	"htmxtest/internal/services"
+
+	"github.com/wcharczuk/go-chart/v2"
 )
 
 var Templs *template.Template
@@ -39,6 +43,29 @@ func RandomMessageHandler(w http.ResponseWriter, r *http.Request) {
 	Render(w, "message", message)
 }
 
+func Graph(w http.ResponseWriter, r *http.Request) {
+	graph := chart.BarChart{
+		Title: "Exemple de graphique",
+		Bars: []chart.Value{
+			{Value: rand.Float64(), Label: "A"},
+			{Value: rand.Float64(), Label: "B"},
+			{Value: rand.Float64(), Label: "C"},
+		},
+	}
+
+	// Buffer pour l'image
+	buffer := bytes.NewBuffer([]byte{})
+	err := graph.Render(chart.PNG, buffer)
+	if err != nil {
+		http.Error(w, "Erreur lors de la génération du graphique", http.StatusInternalServerError)
+		return
+	}
+
+	// Définir le type de contenu et envoyer l'image
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(buffer.Bytes())
+}
+
 func handleError(w http.ResponseWriter, err error, msg string, code int) {
 	log.Printf("%s: %v", msg, err)
 	http.Error(w, msg+": "+err.Error(), code)
@@ -50,4 +77,10 @@ func Render(w http.ResponseWriter, tmpl string, data any) {
 		handleError(w, err, "Error executing template", http.StatusInternalServerError)
 		return
 	}
+}
+
+func ShowGraph(w http.ResponseWriter, r *http.Request) {
+
+	randomval := rand.Intn(100)
+	Render(w, "graph", randomval)
 }
